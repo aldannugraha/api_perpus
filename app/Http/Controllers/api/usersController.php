560168a5f430d0eb896af687341b7f5abd6221ca
Aldan <115200416+aldannugraha@users.noreply.github.com>
2024-02-29 09:10:40 +0700

    Update usersController.php

--- app/Http/Controllers/api/usersController.php
+++ app/Http/Controllers/api/usersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use App\Models\users;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class usersController extends Controller
 {
@@ -13,12 +14,27 @@ class usersController extends Controller
      */
     public function index()
     {
-        $data = users::orderBy('id')->get();
-        return response()->json([
-            'status'=>true,
-            'message'=>'Data ditemukan',
-            'data'=>$data
-        ],200);
+        try {
+            $username = $this->postField('username');
+            $password = $this->postField('password');
+
+            $user = Users::with([])
+                ->where('username', '=', $username)
+                ->where('role', '=', 'PEMINJAM')
+                ->first();
+            if (!$user) {
+                return $this->jsonNotFoundResponse('user not found');
+            }
+
+            $isPasswordValid = Hash::check($password, $user->password);
+            if (!$isPasswordValid) {
+                return $this->jsonUnauthorizedResponse('password did not match');
+            }
+
+            return $this->jsonSuccessResponse('Login Success',$user);
+        }catch (\Throwable $e) {
+            return $this->jsonErrorResponse('internal server error '.$e->getMessage());
+        }
     }
 
     /**
@@ -26,7 +42,21 @@ class usersController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $body = $this->parseRequestBody();
+            $data = [
+                'nama' => $body['nama'],
+                'username' => $body['username'],
+                'telp' => $body['telp'],
+                'alamat' => $body['alamat'],
+                'role' => 'PEMINJAM',
+                'password' => Hash::make($body['password']),
+            ];
+            $occupant = Users::create($data);
+            return $this->jsonCreatedResponse('success', $occupant);
+        } catch (\Throwable $e) {
+            return $this->jsonErrorResponse('internal server error ' . $e->getMessage());
+        }
     }
 
     /**
