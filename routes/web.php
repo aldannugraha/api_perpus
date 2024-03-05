ad40fc37bcc7f9f5e1876d3a8fe689abf5f84e2a
Aldan <115200416+aldannugraha@users.noreply.github.com>
2024-03-05 09:31:36 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,5 @@ use App\Http\Controllers\api\usersController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-$router->get('/', function () use ($router) {
-    return $router->app->version();
-});
 
-$router->group(['prefix' => 'api/v1'], function () use ($router) {
-
-    //public routing
-    $router->group(['prefix' => 'user'], function () use ($router) {
-        $router->post('/login', 'usersController@login');
-        $router->post('/register', 'usersController@store');
-        $router->get('/', 'usersController@index');
-    });
 
