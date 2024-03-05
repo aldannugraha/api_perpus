a732a51f7bb016d97c8f33b49ba33526e34aec82
Aldan <115200416+aldannugraha@users.noreply.github.com>
2024-03-05 08:15:43 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -26,8 +26,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 // Route::post('buku',[booksController::class, 'store']);
 // Route::put('buku/{id}',[booksController::class, 'update']);
 // Route::delete('buku/{id}',[booksController::class, 'destroy']);
-Route::apiResource('buku', booksController::class);
-Route::post('login',[usersController::class, 'login']);
-Route::post('login',[usersController::class, 'store']);
-Route::get('login/show',[usersController::class, 'index']);
+Route::apiResource('/buku', booksController::class);
+Route::post('/login',[usersController::class, 'login']);
+Route::post('/register',[usersController::class, 'store']);
+Route::get('/login/show',[usersController::class, 'index']);
 // Route::apiResource('login', usersController::class);
